ff1ac36dd73d4438a6529486fa844b78c8b70827
Kaba CONDE <kabaconde15@gmail.com>
2019-09-18 22:11:07 +0200

    Change project directory

---
+++ handlers/user.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/kconde2/vote-app/db"
+	"github.com/kconde2/vote-app/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+var DB db.Persist
+
+type User = models.User
+
+func init() {
+	DB = db.PostGresSQL{}
+	var err error
+	DB, err = DB.Connect()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func CreateUser(c *gin.Context) {
+	var u User
+	err := c.BindJSON(&u)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	if err := u.Valid(); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	err = DB.SaveUser(u)
+	if err != nil {
+		log.Println("DB is nil", DB)
+		return
+	}
+
+	c.JSON(http.StatusOK, u)
+}
+
+func GetUser(c *gin.Context) {
+	us, _ := DB.GetUser()
+	c.JSON(http.StatusOK, us)
+}
