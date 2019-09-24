eb3a32982456dbbfa7f2c00bea45623bc53ebdad
Adam Malick Sow <adam.sow97@gmail.com>
2019-09-24 13:59:20 +0200

    login first step

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -4,17 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/appleboy/gin-jwt/v2"
 	"time"
+	"log"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/kconde2/vote-app/api/models"
+	"github.com/kconde2/vote-app/api/db"
 )
 
 type login struct {
-	Username string `form:"username" json:"username" binding:"required"`
+	Email string `form:"email" json:"email" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
 }
 
 var identityKey = "uuid"
-
 
 // AuthMiddleware : The authmiddlare handling jwt token security
 func AuthMiddleware()  (*jwt.GinJWTMiddleware, error){
@@ -51,22 +53,39 @@ func authorizator (data interface{}, c *gin.Context) bool {
 }
 
 func authenticator (c *gin.Context) (interface{}, error) {
-			var loginVals login
-			if err := c.ShouldBind(&loginVals); err != nil {
-				return "", jwt.ErrMissingLoginValues
-			}
-			userID := loginVals.Username
-			password := loginVals.Password
+		var loginVals login
+		var user models.User
+		var db = db.GetDB()
 
-			if (userID == "admin@gmail.com" && password == "admin") || (userID == "test@gmail.com" && password == "test") {
-				return &models.User{
-					Email:  userID,
-					LastName:  "Bo-Yi",
-					FirstName: "Wu",
-				}, nil
-			}
+		if err := c.ShouldBind(&loginVals); err != nil {
+			return "", jwt.ErrMissingLoginValues
+		}
+		// request fields
+		email := loginVals.Email
+		password := loginVals.Password
+		hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+		db.Where("email = ?", email).First(&user)
+		log.Println("err", err)
+		log.Println(user.Password)
+		log.Println(hashPassword)
 
-			return nil, jwt.ErrFailedAuthentication
+		// // Compare the stored hashed password, with the hashed version of the password that was received
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+			// If the two passwords don't match, return a 401 status
+			return nil,jwt.ErrFailedAuthentication
+			// return 
+		}
+
+		// Administrator and test user
+		if (email == "admin@gmail.com" && password == "admin") || (email == "test@gmail.com" && password == "test") {
+			return &models.User{
+				Email:  email,
+				LastName:  "Bo-Yi",
+				FirstName: "Wu",
+			}, nil
+		}
+
+		return nil, jwt.ErrFailedAuthentication
 }
 
 func identityHandler (c *gin.Context) interface{} {
@@ -86,6 +105,7 @@ func payloadFunc (data interface{}) jwt.MapClaims {
 }
 
 // handle user login
-func LoginHandler (login string, password string){
+// func LoginHandler (c *gin.Context){
 
-}
+// 	var creds login
+// }
